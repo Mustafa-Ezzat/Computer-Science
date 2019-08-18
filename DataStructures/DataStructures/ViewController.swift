8464352323d201584797952f0f98927c1217c309
Trufla <mustafa.fci.cs@gmail.com>
2019-08-18 14:58:49 +0200

    Create Data Structures App

---
+++ DataStructures/DataStructures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataStructures
+//
+//  Created by Trufla on 8/18/19.
+//  Copyright © 2019 Trufla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
