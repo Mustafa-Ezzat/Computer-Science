3c690e0323eee29ed0510e56887b8c00a1bb7b46
Trufla <mustafa.fci.cs@gmail.com>
2019-08-18 19:19:48 +0200

    Stack with unit test

--- DataStructures/DataStructures/Sources/Stack.swift
+++ DataStructures/DataStructures/Sources/Stack.swift
@@ -8,6 +8,48 @@
 
 import Foundation
 
-struct Stack<Element> {
+struct Stack<Element: Equatable>: Equatable {
+    
+    private var storage: [Element]
 
+    //empty
+    var isEmpty: Bool {
+        return peak() == nil
+    }
+    
+    init() {
+        storage = []
+    }
+    
+    init(_ elements: [Element]) {
+        storage = elements
+    }
+    
+    //peak
+    func peak() -> Element? {
+        return storage.last
+    }
+    //Push
+    mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    //pop
+    @discardableResult
+    mutating func pop() -> Element? {
+        return storage.popLast()
+    }
+}
+
+extension Stack: CustomStringConvertible {
+    var description: String {
+        return storage
+            .map{"\($0)"}
+            .joined(separator: " ")
+    }
+}
+
+extension Stack: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        storage = elements
+    }
 }
