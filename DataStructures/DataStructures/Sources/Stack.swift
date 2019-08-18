50bb4c857caef499f54d0e78c821a648806e63cd
Trufla <mustafa.fci.cs@gmail.com>
2019-08-18 15:01:30 +0200

    Create Stack file

---
+++ DataStructures/DataStructures/Sources/Stack.swift
@@ -0,0 +1,13 @@
+//
+//  Stack.swift
+//  DataStructures
+//
+//  Created by Trufla on 8/18/19.
+//  Copyright © 2019 Trufla. All rights reserved.
+//
+
+import Foundation
+
+struct Stack<Element> {
+
+}
